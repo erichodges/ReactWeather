bee13eb73d124f6aff23836e7e3f9d7d889e4571
Eric Hodges <ehodges2004@gmail.com>
2016-05-15 15:59:48 -0700

    updated server for heroku

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require ('express');
 
+// create app
 var app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(function (req, res, next) {
+	if (req.headers ['x-forwarded-proto'] === 'https') {
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+
+});
 
 app.use(express.static('public'));
 
-app.listen(8000, function() {
-	console.log('Express server is up on port 8000');
+app.listen(PORT, function() {
+	console.log('Express server is up on port' + PORT);
 });
